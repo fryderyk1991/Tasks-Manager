fb9f2fb387b1b80a0b8cb4a797dfc98666e7b633
Fryderyk Jellinek <fryderyk.jellinek@gmail.com>
2023-11-06 22:04:57 +0100

    add fetchTask file

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,17 +1,40 @@
 import React from 'react';
 
+import { get, create } from '../fetchTask'
 class TasksManager extends React.Component {
     state = {
+        name: '',
         tasks: [],
     }
-
-    onClick = () => {
-        const { tasks } = this.state;
-        console.log( tasks)
+    
+    inputChange = e => {
+        const value = e.target.value;
+        this.setState({
+            name: value, 
+        })
     }
-
+    submitHandler = e => {
+        e.preventDefault();
+        const { name } = this.state;
+        if(name) {
+            // tutaj kod z fetchTask
+           create(name)
+           this.setState({
+             name: "",
+           })
+        }
+    }
     render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
+        const { name } = this.state
+        return (
+            <div className='form__container'>
+            <form className='form' onSubmit={ this.submitHandler }>
+                <label className='form__label'>Name</label>
+                <input className='form__input' value={ name } onChange={ this.inputChange }/>
+                <button type='submit' className='form__submit'>Add Task</button>
+            </form>
+            </div>
+        )
     }
 }
 
---
+++ src/fetchTask.js
@@ -0,0 +1,31 @@
+const url = "http://localhost:3005/data";
+
+
+function fetchData(options = {method: 'GET'}, id = '') {
+       const path = url + id;
+       const promise = fetch(path, options);
+       return promise
+       .then(resp => {
+         if(resp.ok) {
+            return resp.json();
+         }
+         return Promise.reject(resp)
+       }).then(resp => {
+         console.log(resp);
+         return resp;
+       })
+       .catch(err => console.log(err))
+}
+
+
+export function load() {
+    return fetchData();
+}
+export function create(data) {
+    const options = {
+        method: 'POST',
+        body: JSON.stringify( data ),
+        headers: { 'Content-Type': 'application/json'},
+    }
+    return fetchData(options)
+}
